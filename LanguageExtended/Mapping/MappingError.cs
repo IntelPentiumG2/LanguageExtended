35917e0762952f68cf33ac4b85a096a563e9afd4
IntelPentiumG2 <loliwaffel@gmail.com>
2025-03-07 17:50:28 +0100

    ✨ feat(MappingError, MappingOptions): enhance documentation for MappingError and MappingOptions classes with detailed summaries and property descriptions

--- LanguageExtended/Mapping/MappingError.cs
+++ LanguageExtended/Mapping/MappingError.cs
@@ -1,15 +1,37 @@
-﻿namespace LanguageExtended.Mapping;
-
-public class MappingError
-{
-    public string Message { get; }
-    public string PropertyPath { get; }
-    public Exception? Exception { get; }
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+namespace LanguageExtended.Mapping;
     
-    public MappingError(string message, string propertyPath = "", Exception? exception = null)
+    /// <summary>
+    /// Represents an error that occurred during the mapping process.
+    /// </summary>
+    public class MappingError
     {
-        Message = message;
-        PropertyPath = propertyPath;
-        Exception = exception;
+        /// <summary>
+        /// Gets the error message.
+        /// </summary>
+        public string Message { get; }
+    
+        /// <summary>
+        /// Gets the path of the property where the error occurred.
+        /// </summary>
+        public string PropertyPath { get; }
+    
+        /// <summary>
+        /// Gets the exception that caused the error, if any.
+        /// </summary>
+        public Exception? Exception { get; }
+    
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MappingError"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="propertyPath">The path of the property where the error occurred.</param>
+        /// <param name="exception">The exception that caused the error, if any.</param>
+        public MappingError(string message, string propertyPath = "", Exception? exception = null)
+        {
+            Message = message;
+            PropertyPath = propertyPath;
+            Exception = exception;
+        }
     }
-}
--- LanguageExtended/Mapping/MappingOptions.cs
+++ LanguageExtended/Mapping/MappingOptions.cs
@@ -1,8 +1,24 @@
-﻿namespace LanguageExtended.Mapping;
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+namespace LanguageExtended.Mapping;
 
+/// <summary>
+/// Provides configuration options for the mapping process.
+/// </summary>
 public class MappingOptions
 {
+    /// <summary>
+    /// Gets or sets a value indicating whether to ignore case when matching member names.
+    /// </summary>
     public bool IgnoreCase { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to ignore missing members in the target type.
+    /// </summary>
     public bool IgnoreMissingMembers { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to throw an exception on mapping failure.
+    /// </summary>
     public bool ThrowOnMappingFailure { get; set; } = false;
 }
